7dd43c15b8146c2eecb6e63f542c684f51db63dd
Fangfang Zhao <zhaofang123@gmail.com>
2017-02-17 01:03:26 +0800

    创建项目

---
+++ src/main/java/cn/devezhao/commons/App.java
@@ -0,0 +1,13 @@
+package cn.devezhao.commons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
