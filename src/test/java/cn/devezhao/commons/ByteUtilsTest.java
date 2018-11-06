f2c8ffbf19a94625a19866311ffcd5e4e84d4620
FangfangZhao <zhaofang123@gmail.com>
2018-11-06 21:22:21 +0800

    Update src/test/java/cn/devezhao/commons/ByteUtilsTest.java

---
+++ src/test/java/cn/devezhao/commons/ByteUtilsTest.java
@@ -0,0 +1,26 @@
+package cn.devezhao.commons;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 
+ * @author zhaofang123@gmail.com
+ * @since 11/06/2018
+ */
+public class ByteUtilsTest {
+
+	@Test
+	public void testIntHash() {
+		Set<Integer> set = new HashSet<>();
+		for (int i = 0; i < 999999; i++) {
+			int hash = ByteUtils.hash(i);
+			set.add(hash);
+			System.out.println(i + " > " + hash);
+		}
+		Assert.assertTrue(set.size() == 999999);
+	}
+}
